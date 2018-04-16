4719edf2f1b75b6909fe38f1da276a663100f9ba
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-16 20:34:51 +0100

    refactor

--- app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
@@ -3,11 +3,11 @@ package com.example.android.newsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NewsFeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.news_feed_activity);
     }
 }
