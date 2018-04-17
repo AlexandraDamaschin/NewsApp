fe2f389815996bb892e084feb5f1164f3e3e31df
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-17 21:19:23 +0100

    news feed loader

--- app/src/main/java/com/example/android/newsapp/NewsFeedLoader.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeedLoader.java
@@ -1,8 +1,38 @@
 package com.example.android.newsapp;
 
-/**
- * Created by e6420 on 4/16/2018.
- */
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 
-public class NewsFeedLoader {
+import java.util.List;
+
+public class NewsFeedLoader extends AsyncTaskLoader<List<NewsFeed>> {
+    //variables needed
+    //log tag
+    private static final String LOG_TAG = NewsFeedLoader.class.getName();
+    private String mUrl;
+
+    //Constructor
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    //on start loading
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    //This is on a background thread.
+    @Override
+    public List<NewsFeed> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<NewsFeed> newsFeeds = QueryUtils.fetchNewsFeedData(mUrl);
+        return newsFeeds;
+    }
+
 }
