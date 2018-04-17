0d5c23300edf6e5ca6fc0870b4bd78db23c139ea
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-17 21:11:24 +0100

    adapter skeleton

--- app/src/main/java/com/example/android/newsapp/NewsFeedAdapter.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeedAdapter.java
@@ -1,4 +1,29 @@
 package com.example.android.newsapp;
 
-public class NewsFeedAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class NewsFeedAdapter extends ArrayAdapter<NewsFeed> {
+
+    public NewsFeedAdapter(Context context, List<NewsFeed> newsFeeds) {
+        super(context, 0, newsFeeds);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.news_feed_list_item, parent, false);
+        }
+    }
+
+    //implemented methods
+
+
 }
