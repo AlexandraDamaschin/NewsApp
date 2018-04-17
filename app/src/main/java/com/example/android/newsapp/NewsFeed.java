dfd4f5f779ee27945c85892e15f0416a28359dfa
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-17 21:01:14 +0100

    NewsFeed class implemented

--- app/src/main/java/com/example/android/newsapp/NewsFeed.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeed.java
@@ -1,4 +1,37 @@
 package com.example.android.newsapp;
 
 public class NewsFeed {
+    //variables needed
+    private String mTitle;
+    private String mSection;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+    //constructor
+    public NewsFeed(String title, String section, long timeInMilliseconds, String url) {
+        mTitle = title;
+        mSection = section;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    //get title
+    public String getTitle() {
+        return mTitle;
+    }
+
+    //get section
+    public String getSection() {
+        return mSection;
+    }
+
+    //get date
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    //get url
+    public String getUrl() {
+        return mUrl;
+    }
 }
