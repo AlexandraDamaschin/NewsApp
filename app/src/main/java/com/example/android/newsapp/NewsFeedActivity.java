48ff8c03682eed7b0ce67a13d3c50a4a1353a759
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-18 20:55:30 +0100

    activity implemented 1

--- app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
@@ -1,11 +1,20 @@
 package com.example.android.newsapp;
 
 import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class NewsFeedActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsFeed>> {
@@ -27,6 +36,64 @@ public class NewsFeedActivity extends AppCompatActivity implements LoaderManager
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.news_feed_activity);
+
+        //get json data
+        ListView newsFeedListView = (ListView) findViewById(R.id.list);
+
+        //empty list
+        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        newsFeedListView.setEmptyView(mEmptyStateTextView);
+
+        //implement adapter
+        mAdapter = new NewsFeedAdapter(this, new ArrayList<NewsFeed>());
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        newsFeedListView.setAdapter(mAdapter);
+
+        //set an event on click listener
+        //on click on a news open details in new window
+        newsFeedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Find the current newsfeed that was clicked on
+                NewsFeed currentNewsFeed = mAdapter.getItem(position);
+
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri newsFeedUri = Uri.parse(currentNewsFeed.getUrl());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsFeedUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+
+        });
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(NEWSFEED_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_spinner);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
 
     //on create loader
