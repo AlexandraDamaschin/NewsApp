5342960dc7139d7878600e3193783d6b3d3d6a55
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-09 20:59:07 +0100

    second part done

--- app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
+++ app/src/main/java/com/example/android/newsapp/NewsFeedActivity.java
@@ -171,8 +171,8 @@ public class NewsFeedActivity extends AppCompatActivity
         // Append query parameter and its value.
         uriBuilder.appendQueryParameter("q", topic);
         //not to have to old news
-        uriBuilder.appendQueryParameter("from-date","2018-01-01");
-        uriBuilder.appendQueryParameter("show-tags","contributor");
+        uriBuilder.appendQueryParameter("from-date", "2018-01-01");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
         uriBuilder.appendQueryParameter("order-by", orderBy);
 
         return new NewsFeedLoader(this, uriBuilder.toString());
